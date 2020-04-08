4c89cfa4f0dfbe42f8b82d2f01c4a93bed3c37de
robin-j9 <robin@robinjun.com>
2020-04-08 18:29:18 -0500

    changed loop in compound comparer

--- src/Cities/Comparers/CompoundComparer.cs
+++ src/Cities/Comparers/CompoundComparer.cs
@@ -9,9 +9,18 @@ namespace Cities.Comparers
         public IList<IComparer<City>> Comparers { get; set; } = new List<IComparer<City>>();
         public int Compare(City x, City y)
         {
+            //foreach (IComparer<City> comparer in Comparers)
+            //{
+            //    while (comparer.Compare(x, y) != 0)
+            //    {
+            //        return comparer.Compare(x, y);
+            //    }
+            //}
+            //return 0;
+
             foreach (IComparer<City> comparer in Comparers)
             {
-                while (comparer.Compare(x, y) != 0)
+                if (comparer.Compare(x, y) != 0)
                 {
                     return comparer.Compare(x, y);
                 }
